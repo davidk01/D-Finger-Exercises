96ec760ec0a2b41e4415eb9be1bb1720f2b00082
david karapetyan <dkarapetyan@gmail.com>
2012-05-21 20:08:55 -0700

    added unittest workaround

--- linkedlist.d
+++ linkedlist.d
@@ -19,12 +19,12 @@ final class LinkedList(T) {
     return length;
   }
   
-  @property T Head() {
-    return head.value;
+  @property Node Head() {
+    return head;
   }
   
-  @property T Tail() {
-    return tail.value;
+  @property Node Tail() {
+    return tail;
   }
   
   @property T Current() {
@@ -37,7 +37,11 @@ final class LinkedList(T) {
   }
   
   @property bool CanAdvance() {
-    return (current.next !is null);
+    return current.next !is null;
+  }
+  
+  void Rewind() {
+    current = head;
   }
   
   void Advance() {
@@ -46,32 +50,27 @@ final class LinkedList(T) {
     }
   }
   
-  this() {
-    current = head;
-    tail = head;
-  }
-  
   void Insert(T element) {
     if (length == 0) {
       head = this.new Node(element);
+      tail = head;
+      current = head;
     } else {
       auto new_node = this.new Node(element);
       if (current.next is null) {
         current.next = new_node;
         tail = new_node;
-        current = tail;
       } else {
         auto next_element = current.next;
         current.next = new_node;
         new_node.next = next_element;
-        current = new_node;
       }
     }
     length += 1;
   }
   
   void RemoveNext() {
-    if (length > 0 && (current.next !is null)) {
+    if (length > 0 && current.next !is null) {
       auto toLink = current.next.next;
       current.next = toLink;
       if (toLink is null) {
@@ -80,9 +79,39 @@ final class LinkedList(T) {
       length -= 1;
     }
   }
-  
-  unittest {
-    auto linked_list = new LinkedList!(uint);
-    assert(linked_list.Length == 0, "Empty list has non-zero length.");
-  }
 }
+
+//void main() {
+//  auto linked_list = new LinkedList!(uint);
+//  assert(linked_list.Length == 0, "Empty list has non-zero length.");
+//  linked_list.Insert(1);
+//  assert(linked_list.Length == 1, "Adding an element did not increase the length of the list.");
+//  assert(linked_list.Head.value == 1, "Head element does not have value equal to 1.");
+//  assert(linked_list.Tail.value == 1, "Tail element does not have value equal to 1.");
+//  assert(linked_list.Current == 1, "Current element is not point at 1.");
+//  assert(linked_list.Head is linked_list.Tail, "Head and tail should be the same.");
+//  linked_list.Insert(2);
+//  assert(linked_list.Tail.value == 2, "Tail value should be 2.");
+//  assert(linked_list.Current == 1, "Current element should be 1 since we haven't advanced.");
+//  linked_list.Advance();
+//  assert(linked_list.Current == 2, "Current element should be 2 because we just advanced.");
+//  assert(linked_list.Tail.value == 2, "Tail value is still 2.");
+//  linked_list.Insert(3);
+//  assert(linked_list.Tail.value == 3, "We just inserted a new value so tail value should be 3.");
+//  assert(linked_list.Current == 2, "Current value should be 2 since we haven't advanced.");
+//  linked_list.Advance();
+//  assert(linked_list.Current == 3, "Current value should be 3 since we just advanced.");
+//  linked_list.Rewind();
+//  assert(linked_list.Current == linked_list.Head.value, "We just rewound so head and current element must be the same.");
+//  linked_list.RemoveNext();
+//  assert(linked_list.Length == 2, "We just removed an elemnt so length must be 2.");
+//  assert(linked_list.Tail.value == 3, "Tail still points to 3.");
+//  assert(linked_list.Head.next is linked_list.Tail, "Head should be pointing at tail.");
+//  linked_list.Insert(2);
+//  assert(linked_list.Head.next !is linked_list.Tail, "Head should not point to tail anymore.");
+//  linked_list.Advance();
+//  assert(linked_list.Current == 2, "We just advanced to the second element which should be 2.");
+//  linked_list.RemoveNext();
+//  assert(linked_list.Tail.value == 2, "Tail should be pointint to 2 since 3 is gone.");
+//  assert(linked_list.Head.next is linked_list.Tail, "In a two element list head must point to tail.");
+//}
